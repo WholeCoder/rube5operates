b450456209b85d8de972d110c6e35b18f55cd8ac
rpierich@hotmail.com <rpierich@hotmail.com>
2022-10-02 19:16:38 -0400

    addd to finite compression code.

--- compress_finitely.go
+++ compress_finitely.go
@@ -12,25 +12,77 @@ func main() {
 	half := whole / 2
 	quarter := whole / 4
 
+	n := 1
 	X := []byte{'A', 'B'}
 	r := []byte{2, 2}
 	R := 0
-	for i := 0; i < len(r); i++ {
+	for i := 0; i <= n; i++ {
 		R += r[i]
 	}
 	p = []float64{}
-	for i := 0; i < len(r); i++ {
+	for i := 0; i <= n; i++ {
 		p = append(p, float64(r[i])/R)
 	}
 	EOF := 0
-	x = "BABAB"
+	x = "BABAB" + string([]byte{0})
+	k := 5
 
 	c = []byte{0}
-	for i := 1; i < len(p); i++ {
+	for j := 1; j <= n; j++ {
 		sum := 0
-		for j := 1
+		for i := 1; i <= j-1; j++ {
+			sum += r[i]
+		}
+		c = append(c, sum)
 	}
 
+	d := []byte{}
+	for j := 0; j <= n; j++ {
+		d = append(d, c[j]+r[j])
+	}
+
+
+	// algorithm starts
+	s := 0
+	// x is string input
+	a := 0; b := whole
+
+	for i := 1; i <= k+1 {
+		w := b - a
+		b = a + math.Round(w*d[i]/R) // change - d[i] wrong
+		a = a + math.Round(w*c[i]/R) // change - c[i] wrong
+		for b < half || a > half {
+			if b < half {
+				fmt.Println(0)
+				for sidx := 0; sidx < s; sidx++ {
+					fmt.Println(1)
+				}
+			} else if a > half {
+				fmt.Println(1)
+				for sidx := 0; sidx < s; sidx++ {
+					fmt.Println(0)
+				}
+				s = 0
+				a = 2*(a - half); b = 2*(b-half)
+			}
+			for a > quarter && b < 3*quarter {
+				s = s + 1
+				a = 2*(a - quarter); b = 2*(b-quarter)
+			}
+		}
+	}
+	s = s + 1
+	if a <= quarter {
+		fmt.Println(0)
+		for i := 0 ;i < s; i++ {
+			fmt.Println(1)
+		}
+	} else {
+		fmt.Print(1)
+		for i := 0; i < s; i++ {
+			fmt.Println(0)
+		}
+	}
+	}
 	fmt.Println("Hello Universe!")
 }
-
