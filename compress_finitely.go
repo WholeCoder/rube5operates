c6e91c39e43850c246536b332d35c713a9d727f5
rpierich@hotmail.com <rpierich@hotmail.com>
2022-10-02 18:42:45 -0400

    Coding finite compression.

---
+++ compress_finitely.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	precision := 32
+	whole := math.Pos(2, precision)
+	half := whole / 2
+	quarter := whole / 4
+
+	X := []byte{'A', 'B'}
+	r := []byte{2, 2}
+	R := 0
+	for i := 0; i < len(r); i++ {
+		R += r[i]
+	}
+	p = []float64{}
+	for i := 0; i < len(r); i++ {
+		p = append(p, float64(r[i])/R)
+	}
+	EOF := 0
+	x = "BABAB"
+
+	c = []byte{0}
+	for i := 1; i < len(p); i++ {
+		sum := 0
+		for j := 1
+	}
+
+	fmt.Println("Hello Universe!")
+}
+
